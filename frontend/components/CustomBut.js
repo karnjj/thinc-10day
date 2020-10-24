51cab899b2390164548b3694b279cb44cbe50c6e
Karnjj <karnjj@hotmail.com>
2020-10-24 19:24:37 +0700

    💄Update resposive

--- frontend/components/CustomBut.js
+++ frontend/components/CustomBut.js
@@ -1,6 +1,8 @@
 const { Button, makeStyles, fade } = require("@material-ui/core")
 const useStyles = makeStyles((theme) => ({
     button : {
+        width: theme.spacing(16),
+        height: theme.spacing(16),
         '&:hover': {
             background: fade(theme.palette.secondary.main, 0.10),
          },
--- frontend/components/SelectJob.js
+++ frontend/components/SelectJob.js
@@ -1,55 +1,62 @@
-import { 
+import {
   Button,
+  Paper,
   makeStyles,
   Container,
   Typography,
   Icon,
- } from "@material-ui/core";
+  Grid,
+} from "@material-ui/core";
 import { Fragment } from "react";
-import CustomBut from './CustomBut'
-import { Audiotrack, BusinessCenter, Code, Gesture, MovieFilter, Subject } from '@material-ui/icons'
+import CustomBut from "./CustomBut";
+import {
+  Audiotrack,
+  BusinessCenter,
+  Code,
+  Gesture,
+  MovieFilter,
+  Subject,
+} from "@material-ui/icons";
 const useStyles = makeStyles((theme) => ({
   container: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginTop: '48px'
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "48px",
   },
-  papergroup: {
-    display: 'flex',
-    justifyContent: 'center',
-    '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16),
-    },
+  paper: {
+    width: theme.spacing(16),
+    height: theme.spacing(16),
   },
 }));
 
 const JobLinks = [
   //name, icon, paths
-  ['Design', Gesture, ['/']],
-  ['Writting', Subject, ['/']],
-  ['Video', MovieFilter, ['/']],
-  ['Audio', Audiotrack, ['/']],
-  ['Programming', Code, ['/']],
-  ['Business', BusinessCenter, ['/']],
-]
+  ["Design", Gesture, ["/"]],
+  ["Writting", Subject, ["/"]],
+  ["Video", MovieFilter, ["/"]],
+  ["Audio", Audiotrack, ["/"]],
+  ["Programming", Code, ["/"]],
+  ["Business", BusinessCenter, ["/"]],
+];
 
 const SelectJob = () => {
   const classes = useStyles();
   return (
     <Fragment>
       <Container maxWidth="xl" className={classes.container}>
-        <div className={classes.papergroup}>
-          {JobLinks.map(([name, icon, path]) => (
-            <CustomBut href={path}>
-              <Typography>
-                <Icon component={icon} fontSize='large' /><br/>
-                {name}
-              </Typography>
-            </CustomBut>
+        <Grid container justify="center" spacing={2}>
+          {JobLinks.map(([name, icon, path], index) => (
+            <Grid item key={index}>
+              <CustomBut href={path}>
+                <Typography>
+                  <Icon component={icon} fontSize="large" />
+                  <br />
+                  {name}
+                </Typography>
+              </CustomBut>
+            </Grid>
           ))}
-        </div>
+        </Grid>
       </Container>
     </Fragment>
   );
